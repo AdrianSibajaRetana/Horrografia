8e6685b3e8b14b0329580fcc2a9ba8e79ba78065
AdrianSibajaRetana <adriansretana@hotmail.com>
2021-04-20 12:23:10 -0600

    Se movieron los archivos de la librería de datos.
    Cuando se crearon los archivos de la librería estos se generaron fuera de la carpeta del proyecto. Con este commit estos se movieron adentro de la carpeta y se cambiaron las referencias.

---
+++ DataLibrary/DataAccess.cs
@@ -0,0 +1,51 @@
+﻿using Dapper;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLibrary
+{
+    public class DataAccess : IDataAccess
+    {
+        /*
+        Método encargado de leer información de la base de datos.
+        Parámetros: 
+            sql: Código sql a ejecutar.
+            parameters: Tipo genérico que determina los parámetros del código sql a ejecutar.
+            connectionString: Hilera de conexión a la base de datos.
+        Devuelve: 
+            Lista de tipo T, siendo T un modelo de una tabla a especificar. 
+         */
+        public async Task<List<T>> LoadData<T, U>(string sql, U parameters, string connectionString)
+        {
+            using (IDbConnection connection = new MySqlConnection(connectionString))
+            {
+                var rows = await connection.QueryAsync<T>(sql, parameters);
+
+                return rows.ToList();
+            }
+        }
+
+        /*
+        Método encargado de escribir información de la base de datos.
+        Parámetros: 
+            sql: Código sql a ejecutar.
+            parameters: Tipo genérico que determina los parámetros del código sql a ejecutar.
+            connectionString: Hilera de conexión a la base de datos.
+        Devuelve: 
+            Nada
+         */
+        public Task SaveData<T>(string sql, T parameters, string connectionString)
+        {
+            using (IDbConnection connection = new MySqlConnection(connectionString))
+            {
+                return connection.ExecuteAsync(sql, parameters);
+            }
+
+        }
+    }
+}
