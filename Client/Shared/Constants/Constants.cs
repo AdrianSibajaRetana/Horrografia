644382486a4b970a743f270c6fbe8feb9d9eda41
AdrianSibajaRetana <adriansretana@hotmail.com>
2021-05-16 11:20:44 -0600

    Creación de objeto respuesta y constantes
    En el lado del cliente se creó un objeto genérico que almacena la respuesta de los controladores API en el lado del cliente. También se creó una clase para las constantes

---
+++ Client/Shared/Constants/Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Horrografia.Client.Shared.Constants
+{
+    public static class Constants
+    {
+        public const int OKSTATUS = 200;
+        public const int NOTFOUNDSTATUS = 404;
+        public const int INTERNALERRORSTATUS = 500;
+    }
+}
---
+++ Client/Shared/Objects/ControllerResponse.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Horrografia.Client.Shared.Constants;
+namespace Horrografia.Client.Shared.Objects
+{
+    public class ControllerResponse <T>
+    {
+        private List<T> _response = new List<T>();
+        private int _responseStatus;
+
+        public List<T> Response
+        {
+            get => _response;
+            set => _response = value;
+        }
+
+        public int Status
+        {
+            get => _responseStatus;
+            set
+            {
+                if (value == Constants.Constants.OKSTATUS || 
+                    value == Constants.Constants.NOTFOUNDSTATUS || 
+                    value == Constants.Constants.INTERNALERRORSTATUS)
+                {
+                    _responseStatus = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException($"{nameof(value)} must be a valid status.");
+                }
+            }
+        }
+    }
+}
