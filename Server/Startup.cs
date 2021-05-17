85f5cff980850a9e3afd81ef47672d9fa09e3490
AdrianSibajaRetana <adriansretana@hotmail.com>
2021-05-17 14:39:30 -0600

    Se relajaron las condiciones de contraseñas

--- Server/Startup.cs
+++ Server/Startup.cs
@@ -36,7 +36,14 @@ namespace Horrografia.Server
                 options.UseMySql(
                     Configuration.GetConnectionString("DefaultConnection")));
 
-            services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            //Settear a true si quiero confirmar con un correo
+            services.AddDefaultIdentity<ApplicationUser>(options => {
+                options.SignIn.RequireConfirmedAccount = false;
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+            })
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
             services.AddIdentityServer()
