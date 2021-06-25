caa1477071e8a70e5c490e9db0361432fb14858c
AdrianSibajaRetana <adriansretana@hotmail.com>
2021-06-25 14:06:37 -0600

    Creación de modelos DTO para llevar a cago login y registro.
    - Se va a cambiar el login y registro para crear páginas personales desde el lado del cliente.

---
+++ Client/Shared/Objects/ClientModels/ClientUserLoginDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Horrografia.Shared.Models;
+
+namespace Horrografia.Client.Shared.Objects.ClientModels
+{
+    public class ClientUserLoginDTO
+    {
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember me")]
+        public bool RememberMe { get; set; }
+    }
+}
