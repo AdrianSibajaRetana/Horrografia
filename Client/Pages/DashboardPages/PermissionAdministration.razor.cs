4d544680f074a330561864e8fbec0a20d619f340
AdrianSibajaRetana <adriansretana@hotmail.com>
2021-08-06 11:15:10 -0600

    UI para página de permisos creada
    - Se crearon también los componentes para los dos tabs

--- Client/Pages/DashboardPages/PermissionAdministration.razor.cs
+++ Client/Pages/DashboardPages/PermissionAdministration.razor.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Horrografia.Client.Shared.Components.Dashboard.Permission_Administration;
 
 namespace Horrografia.Client.Pages.DashboardPages
 {
