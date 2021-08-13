d86576d2a25245eb28920bfd4647ac5b95e89bb6
AdrianSibajaRetana <adriansretana@hotmail.com>
2021-08-13 13:24:59 -0600

    Mejora de interfaz.
    - Se añadió interfaz para buscar al usuario a quien se le van a cambiar los permisos.

---
+++ Client/Shared/Components/Dashboard/Permission Administration/RolesAsigner.razor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Horrografia.Client.Shared.Components.Dashboard.Permission_Administration
+{
+    public partial class RolesAsigner
+    {
+        private string Busqueda { get; set; }
+    }
+}
