3b82fddb3a0ab026c6f1ab3793b747ef427f651b
AdrianSibajaRetana <adriansretana@hotmail.com>
2021-08-21 10:04:26 -0600

    Creación de página de perfil
    - La página de perfil es capaz de extraer el "user id" del usuario que la accede.
    
    TO DO: Hacer método "Get by user id" en el controlador de usuarios para obtener el UsuarioDTO de la base de datos.

---
+++ Client/Pages/Perfil.razor.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Horrografia.Client.Pages
+{
+
+
+    public partial class Perfil
+    {
+        [Inject]
+        protected AuthenticationStateProvider authProvider { get; set; }
+
+        private bool _isLoading { get; set; }
+        private bool _lecturaExitosa { get; set; }
+        private string id { get; set; }
+        public Perfil()
+        {
+            _isLoading = true;
+            _lecturaExitosa = false;
+        }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await CargarData();
+            _isLoading = false;
+        }
+
+        private async Task CargarData()
+        {
+            AuthenticationState authState = await authProvider.GetAuthenticationStateAsync();
+            var userId = authState.User.Claims.FirstOrDefault(c => c.Type == "sub").Value;
+            id = userId;
+        }
+    }
+}
