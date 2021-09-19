4bf894b82d3814fb9acf54db46e1b5ad720d2cb0
AdrianSibajaRetana <adriansretana@hotmail.com>
2021-09-19 16:44:11 -0600

    Se creó funcionalidad para borrar tags.

--- Client/Shared/Components/Dashboard/ItemAdministration/General/TagManager.razor.cs
+++ Client/Shared/Components/Dashboard/ItemAdministration/General/TagManager.razor.cs
@@ -22,11 +22,17 @@ namespace Horrografia.Client.Shared.Components.Dashboard.ItemAdministration.Gene
         [Parameter]
         public EventCallback<TagModel> OnTagDeletion { get; set; }
 
+        [Parameter]
+        public TagModel TagModelToDelete { get; set; }
+
         private bool _showTagCreationDialog { get; set; }
+
+        private bool _showTagDeletionDialog { get; set; }
 
         public TagManager()
         {
-            _showTagCreationDialog = false; 
+            TagModelToDelete = new(); 
+            _showTagCreationDialog = false;
         }
 
         private int getRelationNumber(int tagId)
@@ -36,12 +42,23 @@ namespace Horrografia.Client.Shared.Components.Dashboard.ItemAdministration.Gene
 
         private void OpenTagCreationDialog()
         {
-            _showTagCreationDialog = true; 
+            _showTagCreationDialog = true;
         }
 
         protected void CloseTagCreationDialog()
         {
-            _showTagCreationDialog = false; 
+            _showTagCreationDialog = false;
+        }
+
+        private void OpenDeletionDialog(TagModel t)
+        {
+            TagModelToDelete = t; 
+            _showTagDeletionDialog = true;
+        }
+
+        protected void CloseDeletionDialog()
+        {
+            _showTagDeletionDialog = false; 
         }
 
         protected async Task CreateTag(TagModel t)
---
+++ Client/Shared/Components/Dashboard/ItemAdministration/TagAdministration/TagDeletionDialog.razor.cs
@@ -0,0 +1,35 @@
+﻿using Horrografia.Shared.Models;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Horrografia.Client.Shared.Components.Dashboard.ItemAdministration.TagAdministration
+{
+    public partial class TagDeletionDialog
+    {
+        [Parameter]
+        public bool ShowDialogue { get; set; }
+
+        [Parameter]
+        public EventCallback OnDialogClosed { get; set; }
+
+        [Parameter]
+        public EventCallback<TagModel> OnTagDeletion { get; set; }
+
+        [Parameter]
+        public TagModel tagToDelete { get; set; }
+
+        private async Task CloseDialog()
+        {
+            await OnDialogClosed.InvokeAsync();
+        }
+
+        private async Task DeleteTag()
+        {
+            await OnTagDeletion.InvokeAsync(tagToDelete);
+            await CloseDialog();
+        }
+    }
+}
