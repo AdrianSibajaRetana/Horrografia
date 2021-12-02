443d096dcb58f6a9b5581ead93400aecf2d7883e
AdrianSibajaRetana <adriansretana@hotmail.com>
2021-12-02 11:44:43 -0600

    Se creó interfaz para las estadísticas.

---
+++ Client/Shared/Components/Dashboard/StatsView.razor.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Horrografia.Client.Shared.Objects.ClientModels;
+using Horrografia.Shared.Models;
+
+namespace Horrografia.Client.Shared.Components.Dashboard
+{
+    public partial class StatsView
+    {
+        enum Opciones
+        {
+            TodasLasPartidas,
+            Año,
+            Mes
+        }
+
+        enum Meses
+        {
+            Enero,
+            Febrero,
+            Marzo,
+            Abril,
+            Mayo,
+            Junio,
+            Julio,
+            Agosto,
+            Septiembre,
+            Octubre,
+            Noviembre,
+            Diciembre
+        }
+
+        private Opciones OpcionEscogida { get; set; }
+        private Meses MesSeleccionado { get; set; }
+        private int AñoSeleccionado { get; set; }
+        
+
+    }
+}
